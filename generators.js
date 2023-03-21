bfbf4502f72221a0677ad0862e643048ed7c3ed0
generate8 <128455622+generate8@users.noreply.github.com>
2023-03-21 12:02:05 +0530

    Create generators.js

---
+++ generators.js
@@ -0,0 +1,16 @@
+/**
+ * @typedef {object} ObjWithValue
+ * @property {any} value
+ */
+
+/**
+ * @typedef {object} Gen    A generator object
+ * @property {()=>ObjWithValue} next      A function that returns the next value
+ * that this generator can generate.
+ *
+ */
+
+/**
+ *just so that this is considered an ES6 module
+ */
+export const junk = 123;
